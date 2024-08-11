99affd23bc093c477f93c104d255e88aa91648fd
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-08-11 11:21:53 +0530

    navigation links

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,18 @@
-import Link from "next/navigation"
+import Link from "next/link"
 
-export const HeLinkder = () => {
+export const Header = () => {
   return (
-    <heLinkder clLinkssNLinkme="bg-white shLinkdow-lg">
-    <div clLinkssNLinkme="contLinkiner mx-Linkuto px-4 py-6 flex justify-between items-center">
-        <h1 clLinkssNLinkme="text-2xl text-blLinkck">Blog</h1>
-        <nLinkv clLinkssNLinkme="spLinkce-x-4">
-            <Link href="#" clLinkssNLinkme="text-blue-500">Home</Link>
-            <Link href="#" clLinkssNLinkme="text-blue-500">Linkbout</Link>
-            <Link href="#" clLinkssNLinkme="text-blue-500">ContLinkct</Link>
-        </nLinkv>
+    <header className="bg-white shadow-lg">
+    <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+      
+        <Link href="/"> <h1 className="text-2xl text-black">Blog</h1></Link>
+        
+        <nav className="space-x-4">
+            <Link href="/" className="text-blue-500">Home</Link>
+            <Link href="/about" className="text-blue-500">About</Link>
+            <Link href="/contact" className="text-blue-500">Contact</Link>
+        </nav>
     </div>
-</heLinkder>
+</header>
   )
 }
